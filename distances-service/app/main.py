c31abf6c78bd99c1c85012a744196882c37f5f69
jeanpaulsb <jeanpaulsierraboom@gmail.com>
2024-11-13 01:11:32 -0500

    add distances microservice to docker compose with fast reload feature

--- distances-service/app/main.py
+++ distances-service/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-
+from .core.database import  init_db
 app = FastAPI()
 
 
+connection = init_db()
+
+print(connection)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
