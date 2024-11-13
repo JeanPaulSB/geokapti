3b4c7a447425a352fcd6cff4d19f761a95085a9b
jeanpaulsb <jeanpaulsierraboom@gmail.com>
2024-11-13 01:39:06 -0500

    add beanie document definition for location

--- locations-service/app/core/database.py
+++ locations-service/app/core/database.py
@@ -1,8 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
+from app.models.locations import Locations
 
 
 async def init_db():
+    # TODO: set as env variable
     CONNECTION_STRING = "mongodb://localhost:27017"
-    client = AsyncIOMotorClient(CONNECTION_STRING)
-    await init_beanie(database=client, document_models=[])
+    client = AsyncIOMotorClient(CONNECTION_STRING)["locations"]
+    await init_beanie(database=client, document_models=[Locations])
