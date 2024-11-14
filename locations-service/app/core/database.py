82b3808aeb4f78afe070fec41fc66053cf79b671
jeanpaulsb <jeanpaulsierraboom@gmail.com>
2024-11-13 20:55:44 -0500

    update db host url and create docker network

--- locations-service/app/core/database.py
+++ locations-service/app/core/database.py
@@ -5,6 +5,6 @@ from app.models.locations import Locations
 
 async def init_db():
     # TODO: set as env variable
-    CONNECTION_STRING = "mongodb://localhost:27017"
+    CONNECTION_STRING = "mongodb://mongodb:27017"
     client = AsyncIOMotorClient(CONNECTION_STRING)["locations"]
     await init_beanie(database=client, document_models=[Locations])
