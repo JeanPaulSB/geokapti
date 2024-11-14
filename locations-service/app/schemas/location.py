71ffce536f0540dd0e59e3b96b61c902c2936869
jeanpaulsb <jeanpaulsierraboom@gmail.com>
2024-11-14 11:09:45 -0500

    add schema for location update

--- locations-service/app/schemas/location.py
+++ locations-service/app/schemas/location.py
@@ -2,10 +2,17 @@ from typing import Optional
 from pydantic import BaseModel
 from bson import ObjectId
 
+
 class Location(BaseModel):
     name: str
     latitude: float
     longitude: float
 
+
 class LocationOut(BaseModel):
     id: str
+
+class LocationUpdate(BaseModel):
+    name: str
+    latitude: float
+    longitude: float
